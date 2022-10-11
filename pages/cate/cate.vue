9f37bf1610f35c40548701e4bb4fcd12be1741bd
Away666 <1143806273@qq.com>
2022-10-11 23:10:13 +0800

    完成了搜索功能的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,30 +1,33 @@
 <template>
-	<view class="scroll-view-container">
-		<!-- 左侧的滚动视图区域 -->
-		<scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
-			<block v-for="(item, i) in cateList" :key="i">
-				<view :class="['left-scroll-view-item', i === active ? 'active' : '']" @click="activeChanged(i)">
-					{{item.cat_name}}
-				</view>
-			</block>
-		</scroll-view>
-		<!-- 右侧的滚动视图区域 -->
-		<scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}" :scroll-top="scrollTop">
-			<view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
-				<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
-				<!-- 动态渲染三级分类的列表数据 -->
-				<view class="cate-lv3-list">
-					<!-- 三级分类 Item 项 -->
-					<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3"
-						@click="gotoGoodsList(item3)">
-						<!-- 图片 -->
-						<image :src="item3.cat_icon"></image>
-						<!-- 文本 -->
-						<text>{{item3.cat_name}}</text>
+	<view class="">
+		<my-search @click="gotoSearch"></my-search>
+		<view class="scroll-view-container">
+			<!-- 左侧的滚动视图区域 -->
+			<scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
+				<block v-for="(item, i) in cateList" :key="i">
+					<view :class="['left-scroll-view-item', i === active ? 'active' : '']" @click="activeChanged(i)">
+						{{item.cat_name}}
+					</view>
+				</block>
+			</scroll-view>
+			<!-- 右侧的滚动视图区域 -->
+			<scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+				<view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
+					<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
+					<!-- 动态渲染三级分类的列表数据 -->
+					<view class="cate-lv3-list">
+						<!-- 三级分类 Item 项 -->
+						<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3"
+							@click="gotoGoodsList(item3)">
+							<!-- 图片 -->
+							<image :src="item3.cat_icon"></image>
+							<!-- 文本 -->
+							<text>{{item3.cat_name}}</text>
+						</view>
 					</view>
 				</view>
-			</view>
-		</scroll-view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
@@ -78,11 +81,16 @@
 				uni.navigateTo({
 					url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
 				})
+			},
+			gotoSearch() {
+				uni.navigateTo({
+					url: '/subpkg/search/search'
+				})
 			}
 		},
 		onLoad() {
 			const sysInfo = uni.getSystemInfo().then(res => {
-				this.wh = res[1].windowHeight
+				this.wh = res[1].windowHeight - 50
 			})
 			this.getCateList()
 		}
@@ -124,6 +132,8 @@
 		}
 
 		.right-scroll-view {
+			background-color: #ffffff;
+
 			.cate-lv2-title {
 				font-size: 12px;
 				font-weight: bold;
---
+++ subpkg/search/search.vue
@@ -0,0 +1,156 @@
+<template>
+	<view class="">
+		<view class="search-box">
+			<!-- 使用 uni-ui 提供的搜索组件 -->
+			<uni-search-bar @input="input" :radius="100" cancelButton="none" focus="true"></uni-search-bar>
+		</view>
+		<!-- 搜索建议列表 -->
+		<view class="sugg-list" v-if="keyword.length !==0">
+			<view class="sugg-item" v-for="(item, i) in searchResults" :key="i" @click="gotoDetail(item.goods_id)">
+				<view class="goods-name">{{item.goods_name}}</view>
+				<uni-icons type="arrowright" size="16"></uni-icons>
+			</view>
+		</view>
+		<!-- 搜索历史 -->
+		<view class="history-box" v-else>
+			<!-- 标题区域 -->
+			<view class="history-title">
+				<text>搜索历史</text>
+				<uni-icons type="trash" size="17" @click="cleanHistory"></uni-icons>
+			</view>
+			<!-- 列表区域 -->
+			<view class="history-list">
+				<uni-tag :text="item" v-for="(item, i) in historys" :key="i" @click="gotoGoodsList(item)"></uni-tag>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				keyword: '',
+				timer: null,
+				searchResults: [],
+				historyList: ['a', 'app', 'apple']
+			};
+		},
+		computed: {
+			historys() {
+				// 注意：由于数组是引用类型，所以不要直接基于原数组调用 reverse 方法，以免修改原数组中元素的顺序
+				// 而是应该新建一个内存无关的数组，再进行 reverse 反转
+				return [...this.historyList].reverse()
+			}
+		},
+		methods: {
+			getSearchList() {
+				if (this.keyword === '') {
+					this.searchResults = []
+					return
+				}
+				uni.$http.get('/api/public/v1/goods/qsearch', {
+					query: this.keyword
+				}).then((res) => {
+					if (res.statusCode === 200) {
+						this.searchResults = res.data.message
+					} else {
+						return uni.$showMsg()
+					}
+				})
+				this.saveSearchHistory()
+			},
+			input(e) {
+				clearTimeout(this.timer)
+				// e.value 是最新的搜索内容
+				this.timer = setTimeout(() => {
+					this.keyword = e
+					this.getSearchList()
+				}, 500)
+			},
+			gotoDetail(goods_id) {
+				uni.navigateTo({
+					// 指定详情页面的 URL 地址，并传递 goods_id 参数
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
+				})
+			},
+			saveSearchHistory() {
+				const set = new Set(this.historyList)
+				set.delete(this.keyword)
+				set.add(this.keyword)
+				this.historyList = Array.from(set)
+				// 调用 uni.setStorageSync(key, value) 将搜索历史记录持久化存储到本地
+				uni.setStorageSync('keyword', JSON.stringify(this.historyList))
+			},
+			cleanHistory() {
+				// 清空 data 中保存的搜索历史
+				this.historyList = []
+				// 清空本地存储中记录的搜索历史
+				uni.setStorageSync('keyword', '[]')
+			},
+			gotoGoodsList(keyword) {
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?query=' + keyword
+				})
+			}
+		},
+		onLoad() {
+			this.historyList = JSON.parse(uni.getStorageSync('keyword') || '[]')
+		}
+	}
+</script>
+
+<style lang="scss">
+	.search-box {
+		position: sticky;
+		top: 0;
+		z-index: 999;
+		background-color: #C00000;
+	}
+
+	.sugg-list {
+		padding: 0 5px;
+
+		.sugg-item {
+			font-size: 12px;
+			padding: 13px 0;
+			border-bottom: 1px solid #efefef;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+
+			.goods-name {
+				// 文字不允许换行（单行文本）
+				white-space: nowrap;
+				// 溢出部分隐藏
+				overflow: hidden;
+				// 文本溢出后，使用 ... 代替
+				text-overflow: ellipsis;
+				margin-right: 3px;
+			}
+		}
+	}
+
+	.history-box {
+		padding: 0 5px;
+
+		.history-title {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			height: 40px;
+			font-size: 13px;
+			border-bottom: 1px solid #efefef;
+		}
+
+		.history-list {
+			display: flex;
+			flex-wrap: wrap;
+
+			.uni-tag {
+				margin-top: 5px;
+				margin-right: 5px;
+			}
+		}
+	}
+</style>
