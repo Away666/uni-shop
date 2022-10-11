050baf008da65edf882d904f91c837703084e64c
Away666 <1143806273@qq.com>
2022-10-11 18:11:25 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {
+	$http
+} from '@escook/request-miniprogram'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+uni.$http = $http
+
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
